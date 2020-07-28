75c6d78b1d02f98cd9a18a66069a14a0ba496c36
jskierbi <jskierbi@gmail.com>
2020-07-28 12:35:42 +0200

    Added GELF

---
+++ README.md
@@ -0,0 +1,33 @@
+## Run ELK stack
+
+```
+docker-compose up
+```
+
+Kibana: `localhost:5601`
+
+Logstash configuration: [docker-elk/pipeline/logstash.conf](docker-elk/pipeline/logstash.conf)
+* listens on port 12201 for GELF messages
+* Prints all incomming messages to console
+* Forwards all incomming messages to Elasticsearch
+
+## Send test message to GELF
+
+Then from terminal:
+```bash
+for i in {1..10} ; do echo '{"version": "1.1","host":"david.org","short_message":"A short message that helps you identify what is going on","full_message":"Backtrace here\n\nmore stuff","level":1,"_user_id":9001,"_some_info":"foo","_some_env_var":"bar"}' | nc -w 1 -u localhost 12201  ; done
+```
+
+## Log4j GELF appender configuration
+
+File: [src/main/java/resources/log4j2.properties](src/main/java/resources/log4j2.properties)
+
+```properties
+appender.gelf.type = Gelf
+appender.gelf.name = GELF
+appender.gelf.host = udp:localhost
+appender.gelf.port = 12201
+appender.gelf.extractStackTrace = true
+appender.gelf.filterStackTrace = true
+```
+
--- src/main/java/com/jskierbi/logplayground/Main.kt
+++ src/main/java/com/jskierbi/logplayground/Main.kt
@@ -1,10 +1,35 @@
 package com.jskierbi.logplayground
 
 import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 
 
-val logger = LogManager.getLogger()
+val logger1 = LogManager.getLogger("something.else")
+val logger2 = LogManager.getLogger("com.jskierbi")
+val logger3 = LogManager.getLogger("com.jskierbi.test")
+class Something(
+        val logger: Logger = LogManager.getLogger(Something::class.java)
+) 
 
 fun main(args: Array<String>) {
-    logger.debug(">>> Hello, world!")
+    logger1.debug("[1] DEBUG")
+    logger1.info("[1] INFO")
+    logger1.warn("[1] WARN")
+    logger1.error("[1] ERROR")
+
+    logger2.debug("[2] DEBUG")
+    logger2.info("[2] INFO")
+    logger2.warn("[2] WARN")
+    logger2.error("[2] ERROR")
+    
+    logger3.debug("[3] DEBUG")
+    logger3.info("[3] INFO")
+    logger3.warn("[3] WARN")
+    logger3.error("[3] ERROR")
+    
+    val something = Something()
+    something.logger.debug("[S] DEBUG")
+    something.logger.info("[S] INFO")
+    something.logger.warn("[S] WARN")
+    something.logger.error("[S] ERROR")
 }
